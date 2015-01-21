41a14cd62fc37a309de343b8c7481b14a3cb4e20
Rajas Shah <rajas.s.shah@gmail.com>
2015-01-21 12:05:06 +0530

    Django rest init
    Signed-off-by: Rajas Shah <rajas.s.shah@gmail.com>
    
    Signed-off-by: Rajas Shah <rajas.s.shah@gmail.com>

---
+++ tutorial/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tutorial.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    # Expected from tutorial
+    # urlpatterns =[ <code below> ]
+    url(r'^', include ('snippets.urls')),
+)
