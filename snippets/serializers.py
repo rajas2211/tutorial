7a7d9ddef46c27c3085fd0d82f9f6d4e59a36696
Rajas Shah <rajas.s.shah@gmail.com>
2015-01-23 23:37:59 +0530

    Started with ModelSerializer
    Signed-off-by: Rajas Shah <rajas.s.shah@gmail.com>

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,3 +1,6 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+class SnippetSerializer(serializers.Serializer):
+	
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -7,13 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
-    # Expected from tutorial
-    # urlpatterns =[ <code below> ]
-    url(r'^', include ('snippets.urls')),
+    #url(r'^', include('snippets.urls')),
 )
-=======
-)
->>>>>>> 06d66608c253f6cfc500b0b5184a1c9597ef06f1
+
