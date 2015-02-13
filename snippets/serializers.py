52a35873478c65840dbc3034b32578e54aaba2e8
Rajas Shah <rajas.s.shah@gmail.com>
2015-02-14 03:11:26 +0530

    Tutorial 1(Almost complete)
    Signed-off-by: Rajas Shah <rajas.s.shah@gmail.com>

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,5 +2,37 @@ from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+
+class SnippetSerializer(serializers.ModelSerializer):
+	#By using model serializer we can actually concise and less replicating
+
+	class Meta:
+		model=Snippet
+		fields=('id','title','code','linenos','language','style')
+"""
 class SnippetSerializer(serializers.Serializer):
-	
+	pk=serializers.IntegerField(read_only=True)
+	title=serializers.CharField(required=False, allow_blank=True, max_length=100)
+	code=serializers.CharField(style={'base_template':'textarea.html'})
+	linenos=serializers.BooleanField(required=False)
+	language=serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+	style=serializers.ChoiceField(choices=STYLE_CHOICES,default='friendly')
+
+	def create(self, validated_data):
+		
+		#This functions creates a Snippet instance of the given validated data and saves it in database
+		
+		return Snippet.objects.create(**validated_data)
+
+	def update(self, instance, validated_data):
+		
+		#To update the selected instance on db
+		
+		instance.title=validated_data.get('title', instance.title)
+		instance.code=validated_data.get('code',instance.code)
+		instance.linenos=validated_data('linenos',instance.linenos)
+		instance.language=validated_data('language',instance.language)
+		instance.style=validated_data('style',instance.style)
+		instance.save()
+		return instance
+"""
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+	#On receiving HttpResponse, this will be exectuted
+	
+	def __init__(self,data,**kwargs):
+		
+		content=JSONRenderer().render(data)
+		kwargs['content_type']='application/json'
+		super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def snippet_list(request):
+
+	if request.method=='GET':
+		snippets=Snippet.objects.all()
+		serialzer=SnippetSerializer(snippets, many=True)
+		return JSONResponse(serialzer.data)
+
+	elif request.method=='POST':
+		data=JSONParser()parse(request)
+		serialzer=SnippetSerializer(data=data)
+		if serialzer.is_valid():
+			serialzer.save()
+			return JSONResponse(serializer.data,status=201)
+		return JSONResponse(serialzer.errors, status=400)
+
+def snippet_detail(request,pk):
+	
+	try:
+		snippet=Snippet.objects.get(pk=pk)
+	except Snippet.DoesNotExsist:
+		return HttpResponse(status=404)
+
+	if request.method=='GET':
+		serialzer= SnippetSerializer(snippet)
+		return JSONResponse(serializer.data)
+
+	elif request.method=='PUT':
+		data=JSONParser().parse(request)
+		serialzer=SnippetSerializer(snippet,data=data)
+		if serialzer.is_valid():
+			serialzer.save()
+			return JSONResponse(serializer.data)
+		return JSONResponse(serializer.errors, status=400)
+
+	elif request.method=='DELETE':
+		snippet.delete()
+		return HttpResponse(status=204)
+
+
+
